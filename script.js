f96a2e4dde16b91dcd7944882c06d97087a8ecd7
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-05-08 16:51:52 -0400

    add function to make rows and blocks

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const container = document.querySelector(".container");
-const square = document.createElement("div");
-square.classList.add("square");
-container.appendChild(square);
+
+function fillRow() {
+  for (let i = 0; i < 16; i++) {
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+    block.addEventListener("mouseenter", () => {
+      block.classList.add("colored");
+    });
+  }
+}
+
+function createGrid() {
+  for (let i = 0; i < 16; i++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    container.appendChild(row);
+  }
+}
+
+createGrid();
