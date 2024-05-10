f75d6d3f40d9a2c0c581e5249c2def396feec8e9
Gcraft13 <120202358+Gcraft13@users.noreply.github.com>
2024-05-10 16:54:06 -0400

    add logic for reset button. add grid button

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const grid = document.querySelector("#container");
 let blocks = 16;
+let numberOfBlocks = parseInt(
+  prompt(
+    "Please insert the size of the grid you want as a number. Ex: 16 for 16x16"
+  )
+);
 const resetBtn = document.querySelector(".reset");
+const newGridBtn = document.querySelector(".newGrid");
 
 function createDiv() {
   for (let i = 1; i <= blocks; i++) {
@@ -28,5 +34,34 @@ newDiv.forEach((button) => {
 resetBtn.addEventListener("click", function () {
   newDiv.forEach((button) => {
     button.classList.remove("hover");
+  });
+});
+
+function removeDiv() {
+  newDiv.forEach((button) => {
+    button.remove();
+  });
+}
+
+function newGrid(numberOfBlocks) {
+  removeDiv();
+  for (i = 0; i <= numberOfBlocks; i++) {
+    let subDiv = document.createElement("div");
+    subDiv.classList.add("subDiv");
+    for (let j = 1; j < blocks; j++) {
+      let div = document.createElement("div");
+      div.classList.add("box");
+      subDiv.appendChild(div);
+    }
+    grid.appendChild(subDiv);
+  }
+}
+
+newGridBtn.addEventListener("click", function () {
+  newGrid(numberOfBlocks);
+  newDiv.forEach((button) => {
+    button.addEventListener("click", () => {
+      button.classList.add("hover");
+    });
   });
 });
